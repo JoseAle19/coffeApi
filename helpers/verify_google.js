7ed7a7d9fd875b0f97598490e09f38cb27817aab
jose alejandro <josealejandrocc22@gmail.com>
2022-08-28 20:20:59 -0500

    google id

--- helpers/verify_google.js
+++ helpers/verify_google.js
@@ -5,10 +5,10 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 async function googleVerify(token ) {
   const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
+      audience: process.env.GOOGLE_CLIENT_ID,  
+    }
+    );
+    console.log("Este es el client id "+process.env.GOOGLE_CLIENT_ID);
   const {name, email, picture} = ticket.getPayload();
 
 return{
